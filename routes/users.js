a97a9c33bcd8baa3515e6b1860db6abe92f03192
Matheus Icaro <matheusicaro2@hotmail.com>
2018-09-15 11:46:17 -0300

    extend methods get:id, post, put, delete for users.

--- routes/users.js
+++ routes/users.js
@@ -17,10 +17,48 @@ router.get('/', function(req, res, next) {
   client_SERVER_RESTful.get('/users', function(err, request, response, obj) {
     
     assert.ifError(err);
-    res.end(JSON.stringify(obj, null, 2));
+    res.json(obj);
 
   });
+});
+  
+router.get('/:id', function(req, res, next) {
 
+  client_SERVER_RESTful.get(`/users/${req.params.id}`, function(err, request, response, obj) {
+    
+    assert.ifError(err);
+    res.json(obj);
+  });
+
+});
+router.put('/:id', function(req, res, next) {
+
+  client_SERVER_RESTful.put(`/users/${req.params.id}`, req.body, function(err, request, response, obj) {
+    
+    assert.ifError(err);
+    res.json(obj);
+  });
+
+});
+
+router.delete('/:id', function(req, res, next) {
+
+  client_SERVER_RESTful.del(`/users/${req.params.id}`, function(err, request, response, obj) {
+    
+    assert.ifError(err);
+    res.json(obj);
+  });
+
+});
+
+router.post('/', function(req, res, next) {
+
+  client_SERVER_RESTful.post(`/users/`, req.body, function(err, request, response, obj) {
+    
+    assert.ifError(err);
+    res.json(obj);
+  });
+  
 });
 
 module.exports = router;
