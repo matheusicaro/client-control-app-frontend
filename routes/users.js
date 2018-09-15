c6353d5fbbf6be31005fa0117aa1d8ce0dab23c6
Matheus Icaro <matheusicaro2@hotmail.com>
2018-09-15 09:49:14 -0300

    [client -> RESTful] configuring CLIENT server to consume RESTFUL server.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
+// * CONSULT http://restify.com/docs/client-guide/
+
 var express = require('express');
+var restify = require('restify-clients');   // npm install restify-clients --save
+var assert = require('assert');
+
 var router = express.Router();
 
-/* GET users listing. */
+var client_SERVER_RESTful = restify.createJsonClient({
+  url: 'http://localhost:4000'   // inserir endereço do servidor 
+});
+
+// ROTA PARA BUSCAR NO SERVIDOR EXTERNO
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  // mudar a rota onde vai buscar no servidor, seguido do metodo.
+  client_SERVER_RESTful.get('/users', function(err, request, response, obj) {
+    
+    assert.ifError(err);
+    res.end(JSON.stringify(obj, null, 2));
+
+  });
+
 });
 
 module.exports = router;
